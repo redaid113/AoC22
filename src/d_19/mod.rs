eb2da85fc392f799ed004f8fc7047403b29317b2
Aidan Feuerherm <aidan.feuerherm@fullscript.com>
2022-12-19 20:42:50 -0500

    faster

--- src/d_19/mod.rs
+++ src/d_19/mod.rs
@@ -1,5 +1,3 @@
-use std::collections::{HashMap, VecDeque};
-
 type Price = Vec<u32>;
 type Robots = Vec<Price>;
 #[derive(Debug)]
@@ -8,15 +6,15 @@ struct Blueprint {
     robots: Robots,
 }
 
+#[derive(Clone, Debug)]
 struct State {
     robot_count: Vec<u32>,
     ore_count: Vec<u32>,
-    iterations: u32,
 }
 
 fn parse_price(line: &str) -> Price {
     let bits: Vec<&str> = line.split(" ").collect();
-    let mut price = vec![0; 3];
+    let mut price = vec![0; 4];
     for i in 0..bits.len() / 2 {
         let num: u32 = bits[i * 2].parse().unwrap();
         let lode = bits[i * 2 + 1];
@@ -35,7 +33,6 @@ fn parse_id(line: &str) -> u32 {
     line.parse().unwrap()
 }
 
-// Blueprint 1: Each ore robot costs 3 ore. Each clay robot costs 3 ore. Each obsidian robot costs 3 ore and 15 clay. Each geode robot costs 2 ore and 8 obsidian.
 fn parse_blueprint(line: &str) -> Blueprint {
     let formatted = line
         .replace("Blueprint ", "")
@@ -63,103 +60,92 @@ fn parse_input(contents: &str) -> Vec<Blueprint> {
     contents.split('\n').map(parse_blueprint).collect()
 }
 
-fn can_build(price: &Price, ores: &Vec<u32>) -> bool {
-    for i in 0..3 {
-        if price[i] > ores[i] {
-            return false;
+fn ceil(a: u32, b: u32) -> u32 {
+    let div = a.div_euclid(b);
+    let m = a % b;
+    if m > 0 {
+        return div + 1;
+    }
+    return div;
+}
+
+fn calculate_step(price: &Price, ore_count: &Vec<u32>, robot_count: &Vec<u32>) -> u32 {
+    let mut step = 0;
+    for p in 0..3 {
+        if price[p] > 0 && robot_count[p] == 0 {
+            return 0;
+        } else if price[p] > ore_count[p] {
+            step = step.max(ceil(price[p] - ore_count[p], robot_count[p]));
         }
     }
-    true
+    step + 1
 }
 
-fn build_robots(robots: &Robots, ores: &Vec<u32>) -> Vec<Vec<u32>> {
-    if can_build(&robots[3], ores) {
-        return vec![vec![0, 0, 0, 1]];
+fn hit_the_mother_lode(robots: &Robots, max_iteration: usize) -> u32 {
+    let mut iterations: Vec<Vec<State>> = vec![];
+    for _ in 0..=max_iteration {
+        iterations.push(vec![]);
     }
+    iterations[0].push(State {
+        ore_count: vec![0, 0, 0, 0],
+        robot_count: vec![1, 0, 0, 0],
+    });
 
-    if can_build(&robots[2], ores) {
-        return vec![vec![0, 0, 1, 0]];
-    }
-
-    let mut output: Vec<Vec<u32>> = vec![vec![0, 0, 0, 0]];
-    if can_build(&robots[1], ores) {
-        output.push(vec![0, 1, 0, 0]);
-    }
-
-    if can_build(&robots[0], ores) {
-        output.push(vec![1, 0, 0, 0]);
-    }
-    output
-}
-
-fn mine_time(robots: &Robots, max_iteration: u32) -> u32 {
-    let mut queue: VecDeque<State> = VecDeque::new();
-    let mut max = 0;
-
-    let mut max_robots = [0; 3];
+    let mut max_robots = [u32::MAX; 4];
     for i in 0..3 {
         max_robots[i] = robots.iter().map(|r| r[i]).max().unwrap();
     }
 
-    queue.push_back(State {
-        robot_count: vec![1, 0, 0, 0],
-        ore_count: vec![0, 0, 0, 0],
-        iterations: 0,
-    });
+    let mut max = 0;
 
-    let mut cache: HashMap<String, Vec<u32>> = HashMap::new();
-
-    while !queue.is_empty() {
-        let state = queue.pop_front().unwrap();
-        if state.iterations == max_iteration {
-            max = max.max(*state.ore_count.last().unwrap());
-            continue;
-        }
-        if (0..3).any(|i| state.robot_count[i] > max_robots[i]) {
-            continue;
-        }
-
-        let key = format!("{}.{:?}", state.iterations, state.robot_count);
-        if cache.contains_key(&key) {
-            let prev = cache.get(&key).unwrap();
-            let nope = state
-                .ore_count
-                .iter()
-                .enumerate()
-                .all(|(i, o)| *o <= prev[i]);
-            if nope {
+    for i in 0..max_iteration {
+        let geodudes = iterations[i]
+            .iter()
+            .map(|iteration| iteration.robot_count[3])
+            .max();
+        for j in 0..iterations[i].len() {
+            let iteration = iterations[i][j].clone();
+            if iteration.robot_count[3] != geodudes.unwrap()
+                && iteration.robot_count[3] != geodudes.unwrap() - 1
+            {
                 continue;
             }
-        }
-        cache.insert(key, state.ore_count.clone());
+            max = max.max(
+                iteration.ore_count[3]
+                    + iteration.robot_count[3] * (max_iteration as u32 - i as u32),
+            );
+            (0..4).for_each(|r| {
+                if max_robots[r] == iteration.robot_count[r] {
+                    return;
+                }
+                let price = &robots[r];
 
-        let mined = state.robot_count.clone();
-        build_robots(robots, &state.ore_count)
-            .iter()
-            .for_each(|built_robots| {
-                let mut next_ore_count = vec![0; 4];
-                let mut next_robots = vec![0; 4];
-
-                for i in 0..4 {
-                    next_ore_count[i] = mined[i] + state.ore_count[i];
+                let step = calculate_step(price, &iteration.ore_count, &iteration.robot_count);
+                if step == 0 {
+                    return;
                 }
 
-                for i in 0..4 {
-                    next_robots[i] = built_robots[i] + state.robot_count[i];
-
-                    for o in 0..3 {
-                        let ore_cost = robots[i][o];
-                        next_ore_count[o] -= built_robots[i] * ore_cost;
-                    }
+                if i + step as usize > max_iteration {
+                    return;
                 }
 
-                queue.push_back(State {
-                    robot_count: next_robots,
-                    ore_count: next_ore_count,
-                    iterations: state.iterations + 1,
+                let mut ore_count = iteration.ore_count.clone();
+                for o in 0..4 {
+                    ore_count[o] += iteration.robot_count[o] * step - price[o];
+                }
+                let mut robot_count = iteration.robot_count.clone();
+                robot_count[r] += 1;
+
+                max = max.max(ore_count[3]);
+
+                iterations[i + step as usize].push(State {
+                    ore_count,
+                    robot_count,
                 });
             })
+        }
     }
+
     max
 }
 
@@ -168,15 +154,17 @@ fn part1(contents: &str) -> u32 {
 
     blueprints
         .iter()
-        .map(|blueprint| blueprint.id * mine_time(&blueprint.robots, 24))
+        .map(|blueprint| blueprint.id * hit_the_mother_lode(&blueprint.robots, 24))
         .sum()
 }
 
 fn part2(contents: &str) -> u32 {
     let blueprints = parse_input(contents);
-    mine_time(&blueprints[0].robots, 32)
-        * mine_time(&blueprints[1].robots, 32)
-        * mine_time(&blueprints[2].robots, 32)
+    blueprints
+        .iter()
+        .take(3)
+        .map(|blueprint| hit_the_mother_lode(&blueprint.robots, 32))
+        .product()
 }
 
 pub fn run(contents: &str) {
