ba1c4ac6a6a430e6b9b0b62d05fc7727adb0c02d
Aidan Feuerherm <aidan.feuerherm@fullscript.com>
2022-12-22 16:44:54 -0500

    d_23 init

--- src/main.rs
+++ src/main.rs
@@ -24,6 +24,7 @@ mod d_19;
 mod d_20;
 mod d_21;
 mod d_22;
+mod d_23;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -57,6 +58,7 @@ fn main() {
         "d_20" => d_20::run(&contents),
         "d_21" => d_21::run(&contents),
         "d_22" => d_22::run(&contents),
+        "d_23" => d_23::run(&contents),
 
         _ => println!("nothing"),
     }
